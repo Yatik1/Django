59eefe3fb0f5c6da7c47a754469b908778867eab
Yatik1 <yatiksrivastava1@gmail.com>
2024-12-14 12:10:17 +0530

    app urls

--- playground/urls.py
+++ playground/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-# URL configuration
 urlpatterns = [
     path("hello/" , views.say_hello),
-    path("content/", views.hello, name="hello"),
-]   
+    path("bye/", views.say_bye)
+]
