73222aa5eece95fecd2780b699a62290e20407d4
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-06 23:12:34 +0530

    get all projects and get project project view functions

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+from django.core import serializers
+from . import models
 
 # Create your views here.
 def say_hello(request):
@@ -11,3 +13,18 @@ def say_bye(request):
 
 def dyn_params(request,pk): 
     return HttpResponse('Dynamic Param :' + str(pk))
+
+def get_all_projects(request):
+    projects = models.Project.objects.all()
+    json_projects = serializers.serialize('json',projects)
+    return HttpResponse(json_projects, content_type='application/json')
+
+def get_project(request,id):
+    project = models.Project.objects.get(pk=id)
+    print(project)
+
+    tag = project.tag.all()
+    print(tag)
+
+    json_project = serializers.serialize('json',[project])
+    return HttpResponse(json_project,content_type='application/json')
