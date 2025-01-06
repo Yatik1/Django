6a0c13f3b27aa51dfbc594bd47f3aaed33162c6f
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-06 23:12:08 +0530

    urls for views

--- playground/urls.py
+++ playground/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("hello/" , views.say_hello),
     path("bye/", views.say_bye),
-    path("params/<str:pk>/", views.dyn_params)  ## this is for dynamic routing 
+    path("params/<str:pk>/", views.dyn_params),  ## this is for dynamic routing 
+    path("getall/",views.get_all_projects),
+    path("getproject/<str:id>/",views.get_project)
 ]
