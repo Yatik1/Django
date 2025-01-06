ad40407a576ba6269a76bfb3dacfcf2e03ccd08d
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-06 23:41:15 +0530

    form url

--- playground/urls.py
+++ playground/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("bye/", views.say_bye),
     path("params/<str:pk>/", views.dyn_params),  ## this is for dynamic routing 
     path("getall/",views.get_all_projects),
-    path("getproject/<str:id>/",views.get_project)
+    path("getproject/<str:id>/",views.get_project),
+    path("create/",views.create_project,name='create_project')
 ]
