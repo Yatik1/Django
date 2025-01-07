548ec4ed9ebc189cebabba01e7ec01d33e452461
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-07 12:49:19 +0530

    urls name

--- playground/urls.py
+++ playground/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/" , views.say_hello),
-    path("bye/", views.say_bye),
+    path("hello/" , views.say_hello,name="hello"),
+    path("bye/", views.say_bye,name="bye"),
     path("params/<str:pk>/", views.dyn_params),  ## this is for dynamic routing 
-    path("getall/",views.get_all_projects),
+    path("getall/",views.get_all_projects,name="getall"),
     path("getproject/<str:id>/",views.get_project),
     path("create/",views.create_project,name='create_project')
 ]
