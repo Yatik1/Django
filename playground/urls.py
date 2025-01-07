1ed336d8a28f37e4df734cbe8802ad7dceb2b54a
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-07 13:35:03 +0530

    delete and update url

--- playground/urls.py
+++ playground/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("params/<str:pk>/", views.dyn_params),  ## this is for dynamic routing 
     path("getall/",views.get_all_projects,name="getall"),
     path("getproject/<str:id>/",views.get_project),
-    path("create/",views.create_project,name='create_project')
+    path("create/",views.create_project,name='create_project'),
+    path("update/<str:pk>",views.update_project,name='update_project'),
+    path("delete/<str:pk>",views.delete_project,name='delete_project')
 ]
