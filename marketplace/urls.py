f391fc45ec0052a64b7edbcf32e54f1323f6a5d6
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-10 13:16:08 +0530

    url config for static files

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.root),
     path('playground/', include("playground.urls"))
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
