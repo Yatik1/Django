ea1efa4d8e7140a2eecc4b41a7fbed4daec22031
Yatik1 <yatiksrivastava1@gmail.com>
2025-01-10 13:15:56 +0530

    setting to access media

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/' # to render the image on the template
+
 STATICFILES_DIRS = [
     # BASE_DIR / 'static' # or
     os.path.join(BASE_DIR,'static')
